6527f2c843d1df3a8117ebbd1497ec0e06043867
Огай Анатолий <anatoliy.ogay@optimabank.kg>
2022-08-15 13:39:35 +0600

    feat: refresh crypto library

--- mpp-library/feature/auth/build.gradle.kts
+++ mpp-library/feature/auth/build.gradle.kts
@@ -1,24 +1,24 @@
-plugins {
-    id("multiplatform-library-convention")
 
-    kotlin("plugin.serialization")
+plugins {
+	id("multiplatform-library-convention")
+
+	kotlin("plugin.serialization")
 }
 
 dependencies {
-    commonMainImplementation(project(":mpp-library:core"))
-    commonMainImplementation(project(":mpp-library:core:data:network"))
-    commonMainImplementation(project(":mpp-library:core:data:storage"))
-    commonMainImplementation(project(":mpp-library:base"))
+	commonMainImplementation(project(":mpp-library:base"))
+	commonMainImplementation(project(":mpp-library:core"))
+	commonMainImplementation(project(":mpp-library:core:data:network"))
+	commonMainImplementation(project(":mpp-library:core:data:storage"))
 
-    commonMainImplementation(libs.coroutines)
-    commonMainImplementation("io.insert-koin:koin-core:3.1.4")
-    commonMainImplementation(libs.kotlinSerialization)
-    commonMainImplementation(libs.ktorClient)
+	commonMainImplementation(libs.coroutines)
+	commonMainImplementation("io.insert-koin:koin-core:3.1.4")
+	commonMainImplementation(libs.kotlinSerialization)
+	commonMainImplementation(libs.ktorClient)
 
-    commonMainApi(libs.mokoMvvmLiveData)
-    commonMainApi(libs.mokoResources)
-    commonMainApi(libs.mokoErrors)
+	commonMainApi(libs.mokoMvvmLiveData)
+	commonMainApi(libs.mokoResources)
+	commonMainApi(libs.mokoErrors)
 
-    val kryptoVersion = "2.2.0"
-    commonMainImplementation("com.soywiz.korlibs.krypto:krypto:$kryptoVersion")
+	commonMainImplementation("com.soywiz.korlibs.krypto:krypto:3.0.0")
 }
