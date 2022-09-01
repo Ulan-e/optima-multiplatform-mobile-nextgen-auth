550fee7141f218a2a1b422bb43eb448c4deb1097
Огай Анатолий <yagooooa@gmail.com>
2022-09-01 16:35:54 +0600

    feat: android back handler

--- android-app/src/main/java/kg/optima/mobile/android/ui/features/common/MainContainer.kt
+++ android-app/src/main/java/kg/optima/mobile/android/ui/features/common/MainContainer.kt
@@ -1,16 +1,20 @@
 package kg.optima.mobile.android.ui.features.common
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.bottomSheet.LocalBottomSheetNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import kg.optima.mobile.base.presentation.StateMachine
+import kg.optima.mobile.android.ui.SingleActivity
 import kg.optima.mobile.android.ui.base.Router
+import kg.optima.mobile.android.utils.asActivity
+import kg.optima.mobile.base.presentation.StateMachine
 import kg.optima.mobile.design_system.android.ui.bottomsheet.BottomSheetInfo
 import kg.optima.mobile.design_system.android.ui.buttons.model.ButtonView
 import kg.optima.mobile.design_system.android.ui.progressbars.CircularProgress
@@ -29,6 +33,11 @@ fun MainContainer(
 	val navigator = LocalNavigator.currentOrThrow
 	val bottomSheetNavigator = LocalBottomSheetNavigator.current
 
+	if (!navigator.canPop) {
+		val activity = LocalContext.current.asActivity()
+		BackHandler { activity?.finish() }
+	}
+
 	Box(modifier = modifier.fillMaxSize()) {
 		when (mainState) {
 			is StateMachine.State.Loading -> {
@@ -43,22 +52,26 @@ fun MainContainer(
 			is StateMachine.State.Error -> {
 				// TODO process error
 				when (val errorState = mainState) {
-					is StateMachine.State.Error.BaseError -> bottomSheetNavigator.show(BottomSheetInfo(
-						title = errorState.error,
-						buttons = listOf(
-							ButtonView.Primary(
-								modifier = Modifier.fillMaxWidth(),
-								text = "Повторить попытку",
-								color = ComposeColors.Green,
-								onClick = { bottomSheetNavigator.pop() }
+					is StateMachine.State.Error.BaseError -> bottomSheetNavigator.show(
+						info = BottomSheetInfo(
+							title = errorState.error,
+							buttons = listOf(
+								ButtonView.Primary(
+									modifier = Modifier.fillMaxWidth(),
+									text = "Повторить попытку",
+									color = ComposeColors.Green,
+									onClick = { bottomSheetNavigator.pop() }
+								)
 							)
-						)
-					))
+						))
 				}
 			}
 		}
+
 		if (infoState != null) {
 			bottomSheetNavigator.show(infoState)
+		} else {
+			bottomSheetNavigator.hide()
 		}
 
 		content()
