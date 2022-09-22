bd64878419cdafd4adf9fd92a3ab4797a83a60cb
toolGeen <htemohpab@gmail.com>
2022-09-22 10:49:18 +0600

    fix : show loading

--- android-app/src/main/java/kg/optima/mobile/android/ui/features/common/MainContainer.kt
+++ android-app/src/main/java/kg/optima/mobile/android/ui/features/common/MainContainer.kt
@@ -104,6 +104,33 @@ fun MainContainer(
 		sheetState = sheetState,
 	) {
 		Box(modifier = modifier.fillMaxSize()) {
+
+			Column(
+				modifier = Modifier
+					.fillMaxSize()
+					.background(ComposeColors.Background)
+			) {
+				if (toolbarInfo != null) {
+					MainToolbar(toolbarInfo.copy(
+						navigationIcon = (toolbarInfo.navigationIcon ?: NavigationIcon()).copy(
+							onBackClick = { navigator.pop() }
+						)
+					))
+				}
+				val columnModifier = contentModifier
+					.fillMaxSize()
+					.weight(1f, false)
+					.background(ComposeColors.Background)
+				if (scrollable)
+					columnModifier.verticalScroll(rememberScrollState())
+
+				Column(
+					modifier = columnModifier,
+					horizontalAlignment = contentHorizontalAlignment,
+					content = { content { navigator.pop() } },
+				)
+			}
+
 			when (mainState) {
 				is State.StateModel.Loading -> {
 					CircularProgress(modifier = Modifier.align(Alignment.Center))
@@ -137,32 +164,6 @@ fun MainContainer(
 						onBottomSheetHidden = onBottomSheetHidden,
 					)
 				}
-			}
-
-			Column(
-				modifier = Modifier
-					.fillMaxSize()
-					.background(ComposeColors.Background)
-			) {
-				if (toolbarInfo != null) {
-					MainToolbar(toolbarInfo.copy(
-						navigationIcon = (toolbarInfo.navigationIcon ?: NavigationIcon()).copy(
-							onBackClick = { navigator.pop() }
-						)
-					))
-				}
-				val columnModifier = contentModifier
-					.fillMaxSize()
-					.weight(1f, false)
-					.background(ComposeColors.Background)
-				if (scrollable)
-					columnModifier.verticalScroll(rememberScrollState())
-
-				Column(
-					modifier = columnModifier,
-					horizontalAlignment = contentHorizontalAlignment,
-					content = { content { navigator.pop() } },
-				)
 			}
 		}
 	}
