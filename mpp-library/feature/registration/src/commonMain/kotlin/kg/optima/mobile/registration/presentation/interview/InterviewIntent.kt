1a351d3ba1cabf1fa34b3709eed891f5a0b5861b
ToolGeen <htemohpab@gmail.com>
2022-09-23 13:51:38 +0600

    feat: interviewScreen

---
+++ mpp-library/feature/registration/src/commonMain/kotlin/kg/optima/mobile/registration/presentation/interview/InterviewIntent.kt
@@ -0,0 +1,7 @@
+package kg.optima.mobile.registration.presentation.interview
+
+import kg.optima.mobile.base.presentation.Intent
+
+class InterviewIntent(
+	override val state: InterviewState
+) : Intent<Unit>()
---
+++ mpp-library/feature/registration/src/commonMain/kotlin/kg/optima/mobile/registration/presentation/interview/InterviewState.kt
@@ -0,0 +1,7 @@
+package kg.optima.mobile.registration.presentation.interview
+
+import kg.optima.mobile.base.presentation.State
+
+class InterviewState : State<Unit>() {
+	override fun handle(entity: Unit) = Unit
+}
